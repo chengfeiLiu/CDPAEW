492af79082877fd749c4440ffc7e84573ed4bfeb
chengfeiLiu <lcfqssyx@163.com>
2025-01-14 13:18:17 +0800

    Update MACD_.py

--- early-warning models/MACD_.py
+++ early-warning models/MACD_.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 from mplfinance.original_flavor import candlestick_ohlc
 import matplotlib.ticker as ticker
 import yfinance
-yfinance.pdr_override()
-def stock(stock_code): 
-
+def stock(stock_code):
     stock_info = pd.read_csv(filepath_or_buffer='./600036.SS_huicai.csv',encoding='utf-8')
     stock_info = stock_info.reset_index()  
     stock_info = stock_info.astype({"Date": str})    
@@ -20,16 +18,10 @@ def get_indicators(stock_code):
     data_result_write = pd.ExcelWriter("./macd_result_huicai.xlsx")
     data.to_excel(data_result_write)
     data_result_write.close()
-
-    data["ma10"] = talib.MA(data["Close"], timeperiod=10)
-    data["ma30"] = talib.MA(data["Close"], timeperiod=30)
- 
-    data["rsi"] = talib.RSI(data["Close"])
     return data
 
 def industry(dict):
     for key, value in dict.items():
-        # d.iteritems: an iterator over the (key, value) items
         stock_info = get_indicators(key)
         
 if __name__ == '__main__':
