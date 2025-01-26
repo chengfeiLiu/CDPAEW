af72724312151159f716a510c21c5307d803f821
chengfeiLiu <lcfqssyx@163.com>
2025-01-26 17:31:51 +0800

    Update TCN-main.py

--- prediction_model/TCN-main.py
+++ prediction_model/TCN-main.py
@@ -63,8 +63,8 @@ class handler_data(Dataset):
     def __len__(self):
         return len(self.all_fenchen_list)
 batch_size = 16
-d_model=64
-seq_len =32 
+d_model=512
+seq_len =256 
 nhead=4
 d_k=d_v=d_q=32
 n_layers=4*d_model
@@ -90,8 +90,8 @@ class transformers(nn.Module):
     def __init__(self,in_feature,out_feature):
         super(transformers,self).__init__()
         self.linear1 = nn.Linear(1,d_model)
-        self.tcn =  TemporalConvNet(num_channels=[32,16,4,5],num_inputs=11,kernel_size=3)
-        self.linear2 = nn.Linear(32,1)
+        self.tcn =  TemporalConvNet(num_channels=[128,16,4,5],num_inputs=11,kernel_size=3)
+        self.linear2 = nn.Linear(128,1)
     def forward(self,data):
         tcndata=self.tcn(data)
         tgt = self.linear2(tcndata)
